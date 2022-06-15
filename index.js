03028ea4f2d29aede64b772e0edbd7d9bab5f9be
filip5337 <107550751+filip5337@users.noreply.github.com>
2022-06-15 12:50:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+/*var modal = document.getElementById('modalid');
+var closemodal = document.getElementById('modalclose');
+
+closemodal.addEventListener('click', modalid.style.display = 'none');*/
+
+
+function fun1(){
+    modalid.style.display = 'flex';
+}
+
+function fun2(){
+    modalid2.style.display = 'flex';
+}
+
+function fun3(){
+    modalid3.style.display = 'flex';
+}
+
+function fun4(){
+    modalid4.style.display = 'flex';
+}
+
+function fun5(){
+    modalid5.style.display = 'flex';
+}
+
+function fun6(){
+    modalid6.style.display = 'flex';
+}
+
+function fun7(){
+    modalid7.style.display = 'flex';
+}
+
+function fun8(){
+    modalid8.style.display = 'flex';
+}
+
+function fun9(){
+    modalid9.style.display = 'flex';
+}
+
+function fun10(){
+    modalid10.style.display = 'flex';
+}
+
+
+
+
+
+
+
+
